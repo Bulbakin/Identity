92fba5e235c762fa4b1b8e35f5500c44d6110e5d
Bulbakin <julius.kulju@hotmail.fi>
2019-01-02 01:12:13 +0200

    Initial commit

---
+++ dotnet-identity-example/Web/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+
+namespace Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+                var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+
+                Role[] roles = (Role[])Enum.GetValues(typeof(Role));
+
+                foreach(var r in roles)
+                {
+                    var identityRole = new IdentityRole
+                    {
+                        Id = r.GetRoleName(),
+                        Name = r.GetRoleName()
+                    };
+
+                    if(!await roleManager.RoleExistsAsync(roleName: identityRole.Name))
+                    {
+                        var result = await roleManager.CreateAsync(identityRole);
+                        if (!result.Succeeded)
+                        {
+                            throw new Exception("Creating role failed.");
+                        }
+                    }
+                }
+
+                ApplicationUser user = new ApplicationUser
+                {
+                    firstName = "Jane",
+                    lastName = "Doe",
+                    Email = "janedoe@example.com",
+                    UserName = "janedoe@example.com",
+                    LockoutEnabled = false
+                };
+
+                if (await userManager.FindByEmailAsync(user.Email) == null)
+                {
+                    var result = await userManager.CreateAsync(user, password: "5ESTdYB5cyYwA2dKhJqyjPYnKUc&45Ydw^gz^jy&FCV3gxpmDPdaDmxpMkhpp&9TRadU%wQ2TUge!TsYXsh77Qmauan3PEG8!6EP");
+
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception("Creating user failed");
+                    }
+
+                    result = await userManager.AddToRolesAsync(user, roles.Select(r => r.GetRoleName()));
+
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception("Adding user to role failed");
+                    }
+                }
+            }
+
+            host.Run();
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ dotnet-identity-example/Web/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Web.Models;
+
+namespace Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("database")), ServiceLifetime.Scoped);
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options => {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Events = new CookieAuthenticationEvents
+                {
+                    OnRedirectToAccessDenied = ReplaceRedirectorWithStatusCode(HttpStatusCode.Forbidden),
+                    OnRedirectToLogin = ReplaceRedirectorWithStatusCode(HttpStatusCode.Unauthorized)
+                };
+
+                options.Cookie.Name = ".applicationname";
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.None;
+
+                options.SlidingExpiration = true;
+            });
+
+            services.AddMvc();
+        }
+
+        private Func<RedirectContext<CookieAuthenticationOptions>, Task> ReplaceRedirectorWithStatusCode(HttpStatusCode statusCode) => context =>
+        {
+            context.Response.StatusCode = (int)statusCode;
+            return Task.CompletedTask;
+        };
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
